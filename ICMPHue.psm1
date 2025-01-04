4af4fe70e9e9722be0c18437cbbb822efe126455
Kristian Leth <ksl@ctx.dk>
2025-01-04 17:45:10 +0100

    added first function

---
+++ ICMPHue.psm1
@@ -0,0 +1,13 @@
+
+$Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+foreach ($function in @($Public + $Private)) {
+    try {
+        Import-Module $function.FullName -ErrorAction stop
+    }
+    catch {
+        throw "Failed to import $($function.fullname) - $($_.Exception.Message)"
+    }
+}
+Export-ModuleMember -Function $public.Basename
+
---
+++ Public/get-hueroom.ps1
@@ -0,0 +1,13 @@
+function get-HueRoom {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $roomName
+    )
+    $allRooms = Invoke-RestMethod -Method GET -Uri "https://$hueBridge/clip/v2/resource/room" -Headers $global:Header -ContentType "application/json" -SkipCertificateCheck
+    $roomObj = $allRooms.data | Where-Object {$_.metadata.name -eq $roomName}
+    
+    if ($roomObj) {
+        return $roomObj
+    }
+}
